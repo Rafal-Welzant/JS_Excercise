11150ef6a81f1b14ce78363c6bccd24854e47369
Rafal Welzant <welzant@gmail.com>
2022-03-04 12:31:07 +0100

    String

--- scripts.js
+++ scripts.js
@@ -1,7 +1,42 @@
-const name = "Cyryl";
-const petsName = "Fuks";
-let petsAge = 12;
+// const text1 = "powiększ mnie!"
+// console.log(text1.toUpperCase());
 
-console.log(`Cześć, jestem ${name}, a to jest ${petsName} - mój pies`);
-console.log(`${petsName} ma już ${petsAge} lat!`);
-console.log("Cześć, jestem " + name + " a to jest " + petsName + " - " + "mój pies" + ". " + petsName + " ma już " + petsAge + " lat" + "!");
+// const text2 = "ZAPISZ MNIE MAŁYMI LITERAMI";
+// console.log(text2.toLowerCase());
+
+// const text3 = '$#%#^ wytnij te dziwne znaki na początku!';
+// console.log(text3.slice(6));
+
+// const text4 = 'sprawdź, czy zawieram słowo "czy"';
+// console.log(text4.includes('czy'));
+
+// const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+// console.log(text5.charAt(2));
+
+// const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+// console.log(text6.replaceAll("pies","kot"));
+
+// const text7 = 'podziel, ten, string, od, przecinków'
+// console.log(text7.split(","));
+
+
+const text1 = "powiększ mnie!"
+console.log(text1.toUpperCase());
+
+const text2 = "ZAPISZ MNIE MAŁYMI LITERAMI";
+console.log(text2.toLowerCase());
+
+const text3 = '$#%#^ wytnij te dziwne znaki na początku!';
+console.log(text3.slice(6));
+
+const text4 = 'sprawdź, czy zawieram słowo "czy"';
+console.log(text4.includes('czy'));
+
+const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+console.log(text5.charAt(2));
+
+const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+console.log(text6.replaceAll('pies','kot'));
+
+const text7 = 'podziel, ten, string, od, przecinków'
+console.log(text7.split(','));
