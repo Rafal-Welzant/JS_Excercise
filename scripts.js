3775f11a685eed1c5a256d0ad4ed5b61be2cc3cc
Rafal Welzant <welzant@gmail.com>
2022-03-04 10:46:59 +0100

    Template_string

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
 const name = "Cyryl";
+const petsName = "Fuks";
+let petsAge = 12;
 
-console.log(name);
-
-let lastName = "Grzmi";
-console.log(lastName);
-
-
-let age = 36;
-
-console.log(age);
+console.log(`Cześć, jestem ${name}, a to jest ${petsName} - mój pies`);
+console.log(`${petsName} ma już ${petsAge} lat!`);
+console.log("Cześć, jestem " + name + " a to jest " + petsName + " - " + "mój pies" + ". " + petsName + " ma już " + petsAge + " lat" + "!");
